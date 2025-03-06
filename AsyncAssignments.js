fe6a764ae2cea6764de2ecbc3d6e56d9ceff8e4d
Yasar <yasarmansuri16@gmail.com>
2025-03-06 11:01:51 +0530

    Hello!

--- AsyncAssignments.js
+++ AsyncAssignments.js
@@ -1,17 +1,58 @@
-function filterEvenNumbers(arr, isEven) {
-    // Write your code here
-    // Do not console.log, just return the result.
-    let odd=[];
+// Filtering Even Numbers Using Callback Functions
+
+// function filterEvenNumbers(arr, isEven) {
+//     // Write your code here
+//     // Do not console.log, just return the result.
+//     let odd=[];
   
+//     for(let i=0; i<arr.length; i++){
+//       if(!isEven(arr[i])){
+//         odd.push(arr[i]);
+//       }
+//     }
+//     return odd;
+//   }
+  
+//   const isEven = (num) => num % 2 === 0;
+  
+//   const readline = require("readline");
+  
+//   const input = [];
+  
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
+  
+//   rl.on("line", function (cmd) {
+//     input.push(cmd);
+//   });
+  
+//   rl.on("close", function (cmd) {
+//     Main(input);
+//   });
+  
+//   function Main(input) {
+//     const arr = JSON.parse(input[0]).map((e) => parseInt(e, 10));
+//     console.log(filterEvenNumbers(arr, isEven));
+//   }
+
+// Summing Values Using a Callback Function
+
+function sumWithCallback(arr, callback) {
+    // Do not console.log here
+    // Just return the output
+    let sum = 0;
     for(let i=0; i<arr.length; i++){
-      if(!isEven(arr[i])){
-        odd.push(arr[i]);
-      }
+      sum = callback(sum, arr[i]);
     }
-    return odd;
+    return sum;
+    
   }
   
-  const isEven = (num) => num % 2 === 0;
+  function multiplyByTwo(sum,num) {
+    return sum + num * 2;
+  }
   
   const readline = require("readline");
   
@@ -31,7 +72,8 @@ function filterEvenNumbers(arr, isEven) {
   });
   
   function Main(input) {
-    const arr = JSON.parse(input[0]).map((e) => parseInt(e, 10));
-    console.log(filterEvenNumbers(arr, isEven));
+    const arr = JSON.parse(input[0]);
+    console.log(sumWithCallback(arr, multiplyByTwo));
   }
+  
   
