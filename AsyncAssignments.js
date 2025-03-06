447e4d90682822981d45f263dae2ee6596813efd
Yasar <yasarmansuri16@gmail.com>
2025-03-06 11:05:28 +0530

    Hello!

--- AsyncAssignments.js
+++ AsyncAssignments.js
@@ -37,22 +37,68 @@
 //     console.log(filterEvenNumbers(arr, isEven));
 //   }
 
-// Summing Values Using a Callback Function
+// ------------------------------------------------------------------------------------------
 
-function sumWithCallback(arr, callback) {
-    // Do not console.log here
-    // Just return the output
-    let sum = 0;
-    for(let i=0; i<arr.length; i++){
-      sum = callback(sum, arr[i]);
-    }
-    return sum;
+// // Summing Values Using a Callback Function
+
+// function sumWithCallback(arr, callback) {
+//     // Do not console.log here
+//     // Just return the output
+//     let sum = 0;
+//     for(let i=0; i<arr.length; i++){
+//       sum = callback(sum, arr[i]);
+//     }
+//     return sum;
     
+//   }
+  
+//   function multiplyByTwo(sum,num) {
+//     return sum + num * 2;
+//   }
+  
+//   const readline = require("readline");
+  
+//   const input = [];
+  
+//   const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+//   });
+  
+//   rl.on("line", function (cmd) {
+//     input.push(cmd);
+//   });
+  
+//   rl.on("close", function (cmd) {
+//     Main(input);
+//   });
+  
+//   function Main(input) {
+//     const arr = JSON.parse(input[0]);
+//     console.log(sumWithCallback(arr, multiplyByTwo));
+//   }
+  
+//   ------------------------------------------------------------------------------------------
+
+// Sorting Objects by Key Using Callback Functions
+
+function sortObjectsByKey(arr, keyName, compareFunc) {
+    // Do-- not console.log here
+    // Just return the result
+    return arr.sort((a, b) => compareFunc(a[keyName], b[keyName]));
   }
   
-  function multiplyByTwo(sum,num) {
-    return sum + num * 2;
-  }
+  const compareFunc = (a, b) => {
+    if (a < b) {
+      return -1;
+    }
+  
+    if (a > b) {
+      return 1;
+    }
+  
+    return 0;
+  };
   
   const readline = require("readline");
   
@@ -73,7 +119,6 @@ function sumWithCallback(arr, callback) {
   
   function Main(input) {
     const arr = JSON.parse(input[0]);
-    console.log(sumWithCallback(arr, multiplyByTwo));
+    console.log(sortObjectsByKey(arr, input[1], compareFunc));
   }
-  
   
